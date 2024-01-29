fb304571c14e130a8a54b34b602d4a17d4a097bc
HP@LAPTOP <baxtibaybekniyazov@gmail.com>
2024-01-30 00:06:01 +0500

    Make generate more questions

--- js/main.js
+++ js/main.js
@@ -797,25 +797,121 @@ answersDB().forEach((item, index) => {
       <span class="quostion-text">${index + 1}. ${item.quostion}</span>
             <li class="test-item">
               <label>
-                <input type="radio" name="${index}"> ${item.a}
+                <input class="answer-input" type="radio" name="${index}"> ${item.a}
               </label>
             </li>
             <li class="test-item">
               <label>
-                <input type="radio" name="${index}"> ${item.b}
+                <input class="answer-input" type="radio" name="${index}"> ${item.b}
               </label>
             </li>
             <li class="test-item">
               <label>
-                <input type="radio" name="${index}"> ${item.c}
+                <input class="answer-input" type="radio" name="${index}"> ${item.c}
               </label>
             </li>
             <li class="test-item">
               <label>
-                <input type="radio" name="${index}"> ${item.d}
+                <input class="answer-input" type="radio" name="${index}"> ${item.d}
               </label>
             </li>
         </ul>
   `
   form.append(container)
 })
+
+// elHtml
+const elButton = document.querySelector('#generate')
+const elAnswerContent = document.querySelector('.result')
+// const elsAnswers = document.querySelectorAll('.js-buttons')
+// console.log(elsAnswers);
+
+elAnswerContent.innerHTML = ''
+const buttonNumber = []
+
+headerContent()
+counter(25)
+createSendButton('div', 'button-submit', 'Yakunlash', elAnswerContent)
+// When clicked, background changed other color
+if(elButton) {
+  elButton.addEventListener('click', () => {
+    const button = document.createElement('button')
+    button.classList.add('button-primary')
+    button.textContent = 26
+
+    elAnswerContent.appendChild(button)
+  })
+}
+
+// Clicker function
+if(elAnswerContent) {
+  elAnswerContent.addEventListener('click', (evt) => {
+    if(evt.target.matches('.button-primary')) {
+      evt.target.classList.add('background-color')
+    }
+  })
+}
+
+// Generate 50 questions
+if(elAnswerContent) {
+  elAnswerContent.addEventListener('click', (evt) => {
+    if(evt.target.matches('.button-submit')) {
+      elAnswerContent.innerHTML = ''
+
+      headerContent()
+      counter(50)
+      createSendButton('div',
+        'button-submit',
+        'Yakunlash',
+        elAnswerContent
+      )
+    }
+  })
+}
+
+
+// ------------------========= FUNCTIONS START =======-------------
+
+// BUTTON counter
+function counter(count) {
+  for(let i = 1; i <= count; i++) {
+    buttonNumber.push(i)
+    createButton()
+  }
+}
+
+
+// CREAT BUTTON FUNCTION
+function createButton() {
+  const button = document.createElement('button')
+    button.classList.add('button-primary', 'js-buttons')
+    elAnswerContent.appendChild(button)
+
+  buttonNumber.forEach(item => {
+    button.textContent = item
+  })
+}
+
+
+// Content header maker
+function headerContent() {
+  const span = document.createElement('span')
+  span.classList.add('answer')
+
+  span.innerHTML = `
+    <a href="file/Malumotlar bazasi.doc" class="answer__link">Javoblar
+    </a>
+  `
+  elAnswerContent.appendChild(span)
+}
+
+// CREATE SEND BUTTON
+function createSendButton(div, clasName, text, append) {
+  div = document.createElement(div)
+  div.classList.add(clasName)
+  div.style.textAlign = 'center'
+  div.textContent = text
+  append.appendChild(div)
+}
+
+// ------------------========= FUNCTIONS END =======-------------
